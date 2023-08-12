fee77087cd63298a2c83377ba57d4a3b3cb633f9
omnidox <omnidox05@gmail.com>
2023-08-11 22:09:59 -0400

    resetting the cache implementation

--- Computer_Mobilephone_Updated_cached.py
+++ Computer_Mobilephone_Updated_cached.py
@@ -12,8 +12,6 @@ LOCATIONS = ['house']
 
 API_ENDPOINT = "http://127.0.0.1:8084/query"
 
-path_cache = defaultdict(list)
-
 start_time = time.time()  # Record the start time
 
 def fetch_related_data(node, rel_type):
@@ -29,8 +27,8 @@ def fetch_related_data(node, rel_type):
         return {"edges": []}
 
 
-def bfs_with_caching(start, max_degree=2):
-    """BFS function with path caching."""
+def bfs_modified_with_pathwise_visited(start, max_degree=2):
+    """Modified BFS function with tracking, progress updates, and timer."""
     
     # start_time = time.time()  # Record the start time
     
@@ -41,24 +39,18 @@ def bfs_with_caching(start, max_degree=2):
     global_visited = set([start])
     path_counter = 0
     
+    # Introducing the processed_edges set
+    
     try:
         while queue:
             node, path, pathwise_visited, first_edge_weight = queue.popleft()
 
+            # processed_edges = set()
+            
             degree_counter = len(path)
 
             if degree_counter > max_degree:
                 continue
-
-            # Check the cache
-            if node in path_cache:
-                for cached_path, cached_weight in path_cache[node]:
-                    if not any(n[0] for n in cached_path if n[0] in pathwise_visited):
-                        # If the cached path doesn't introduce a cycle
-                        print(f"Found a cached path for {node}!")
-                        paths[node].append((path + cached_path, cached_weight))
-                        continue
-
 
             rel_types = ["AtLocation", "UsedFor"] if degree_counter == 0 else ["RelatedTo", "Synonym", "IsA"]
 
@@ -66,17 +58,20 @@ def bfs_with_caching(start, max_degree=2):
                 data = fetch_related_data(node, rel_type)
 
                 for edge in data["edges"]:
+                    
                     if edge["start"]["language"] != "en" or edge["end"]["language"] != "en":
                         continue
-
+                
                     next_node = None
                     weight = edge["weight"]
                     if weight < 1:
                         continue
                     relation = edge["rel"]["label"]
-
+                    
+                    
                     start_label = edge["start"]["@id"].replace("/c/en/", "")
                     end_label = edge["end"]["@id"].replace("/c/en/", "")
+
 
                     if start_label != node and end_label != node:
                         continue
@@ -86,11 +81,20 @@ def bfs_with_caching(start, max_degree=2):
                     elif relation == "UsedFor" and end_label != node and end_label not in pathwise_visited:
                         next_node = end_label
 
+
+                    elif relation == "Synonym":
+                            if start_label != node and start_label not in pathwise_visited:
+                                next_node = start_label
+                            elif end_label != node and end_label not in pathwise_visited:
+                                next_node = end_label 
+
                     elif relation == "RelatedTo" and weight > 1.5:
-                        if start_label != node and start_label not in pathwise_visited:
-                            next_node = start_label
-                        elif end_label != node and end_label not in pathwise_visited:
-                            next_node = end_label    
+                            # print (weight)
+                            if start_label != node and start_label not in pathwise_visited:
+                                next_node = start_label
+                            elif end_label != node and end_label not in pathwise_visited:
+                                next_node = end_label    
+
 
                     elif relation == "IsA" and start_label != node and start_label not in pathwise_visited:
                         next_node = start_label
@@ -101,17 +105,7 @@ def bfs_with_caching(start, max_degree=2):
                         new_visited.add(next_node)
                         new_path = path + [(node, relation, weight)]
                         current_first_edge_weight = first_edge_weight if first_edge_weight is not None else weight
-
-                        # Cache update for all paths
-                        if next_node not in path_cache:
-                            path_cache[next_node] = [(new_path, current_first_edge_weight)]
-                        else:
-                            path_cache[next_node].append((new_path, current_first_edge_weight))
-
-
                         queue.append((next_node, new_path, new_visited, current_first_edge_weight))
-
-
                         if next_node in OBJECTS:
                             path_counter += 1
                             paths[next_node].append((new_path, current_first_edge_weight))
@@ -128,7 +122,7 @@ def bfs_with_caching(start, max_degree=2):
     except KeyboardInterrupt:
         print("\\nUser interrupted the BFS traversal. Saving any necessary data...")
         return paths
-
+        # raise  # Propagate the exception upwards
 
 # Update the main function to call the new BFS function
 def main():
@@ -136,7 +130,7 @@ def main():
     not_found_objects = set(OBJECTS)
 
     for location in LOCATIONS:
-        location_paths = bfs_with_caching(location)
+        location_paths = bfs_modified_with_pathwise_visited(location)
         for obj, obj_paths in location_paths.items():
             if obj in not_found_objects:
                 not_found_objects.remove(obj)
